b194b9c75d1d66c97ed29b047e2e66703b9757c8
tehila1985 <t0556771985@gmail.com>
2026-02-22 23:27:33 +0200

    changign the rating repository to Ado Netא

--- repository/RatingRepository .cs
+++ repository/RatingRepository .cs
@@ -1,5 +1,5 @@
 ﻿using Entities;
-using Microsoft.Data.SqlClient; // חשוב להוסיף את ה-using הזה
+using Microsoft.Data.SqlClient; 
 using Microsoft.Extensions.Configuration;
 using Repository.Models;
 using System.Data;
@@ -9,8 +9,6 @@ namespace Repository
     public class RatingRepository : IRatingRepository
     {
         private readonly IConfiguration _configuration;
-
-        // מזריקים את ה-Configuration כדי לקבל את ה-ConnectionString
         public RatingRepository(IConfiguration configuration)
         {
             _configuration = configuration;
@@ -18,18 +16,16 @@ namespace Repository
 
         public async Task<Rating> AddRating(Rating newRating)
         {
-            // שליפת מחרוזת החיבור מה-appsettings.json
             string connectionString = _configuration.GetConnectionString("Tehila");
 
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                // שימי לב לשמות העמודות - הם חייבים להיות בדיוק כמו ב-SQL
+        
                 string query = @"INSERT INTO RATING (HOST, METHOD, [PATH], REFERER, USER_AGENT, Record_Date) 
                                 VALUES (@Host, @Method, @Path, @Referer, @UserAgent, @Date)";
 
                 using (SqlCommand cmd = new SqlCommand(query, conn))
                 {
-                    // הוספת הפרמטרים בצורה בטוחה
                     cmd.Parameters.AddWithValue("@Host", newRating.Host ?? (object)DBNull.Value);
                     cmd.Parameters.AddWithValue("@Method", newRating.Method ?? (object)DBNull.Value);
                     cmd.Parameters.AddWithValue("@Path", newRating.Path ?? (object)DBNull.Value);
